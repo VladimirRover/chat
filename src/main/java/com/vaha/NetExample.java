73971352d6f7cd96fb061a8e5b1f10156567c7e6
vakhtang <sogomonyan@alliance-electro.ru>
2019-11-21 14:52:19 +0300

    init commit

--- src/main/java/com/vaha/NetExample.java
+++ src/main/java/com/vaha/NetExample.java
@@ -1,4 +1,36 @@
 package com.vaha;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class NetExample {
+    private static final int DEFAULT_PORT = 10000;
+
+    public static void main(String[] args) {
+        System.out.println("start ...");
+        try {
+            ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
+            Socket socket = serverSocket.accept();
+            System.out.println("Connection: " + socket);
+
+            Scanner scanner = new Scanner(socket.getInputStream());
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                System.out.println(line);
+                writer.println("OK");
+                writer.flush();
+                if (line.equals("bye")){
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
